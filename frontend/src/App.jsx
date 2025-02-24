759b2df8b61bb7150449121f633bf6ebf16408cb
John Penaflor <johnvin.penaflor@gmail.com>
2025-02-24 01:56:45 -0700

    Completed Sign Up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import Login from './pages/login/Login.jsx';
+import SignUp from './pages/signup/Signup.jsx';
 
 function App() {
 
@@ -11,7 +12,8 @@ function App() {
     //full height, padding of 4 on all directions
     //call Login page first
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      {/* <Login/> */}
+      <SignUp/>
       
     </div>
   )
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import GenderCheckBox from "./GenderCheckbox";
 
-function Signup() {
+const SignUp = () => {
   return (
-    <div>
-      SingUp
-    </div>
-  )
-}
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+    <div className='w-full p-6 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <h1 className='text-4xl font-semibold text-center text-gray-300'>
+        Sign Up <span className='text-blue-500'> ChatApp</span>
+      </h1>
 
-export default Signup;
+      <form>
+        {/*Fullname, username, enter password and confirm password */}
+        <div>
+          <label className='label p-2'>
+            <span className='text-base text-gray-200 label-text'>Full Name</span>
+          </label>
+          <input type='text' placeholder='John Doe' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'>
+            <span className='text-base text-gray-200 label-text'>Username</span>
+          </label>
+          <input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'>
+            <span className='text-base text-gray-200 label-text'>Password</span>
+          </label>
+          <input type='text' placeholder='Enter Password' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'>
+            <span className='text-base text-gray-200 label-text'>Confirm Password</span>
+          </label>
+          <input type='text' placeholder='Confirm Password' className='w-full input input-bordered h-10' />
+        </div>
+
+      {/*Gender Checkbox */}
+      <GenderCheckBox />
+
+      <a className='text-sm text-gray-400
+       hover:underline hover:text-blue-400 mt-2 inline block' href='#'>
+        Already have an account?
+      </a>
+      <div className='pt-4'>
+          <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+        </div>
+      </form>
+
+    </div>
+
+  </div>
+  );
+};
+export default SignUp;

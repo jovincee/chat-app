28155d2c4c02d4a33267ea1263e73050919ef603
John Penaflor <johnvin.penaflor@gmail.com>
2025-02-26 05:26:38 -0700

    Get Conversations from Backend complete

--- frontend/src/components/Sidebar/ConvBar.jsx
+++ frontend/src/components/Sidebar/ConvBar.jsx
@@ -1,18 +1,26 @@
+import useConversation from "../../zustand/useConversation";
 
-const ConvBar = () => {
+const ConvBar = ({conversation,lastIdx,emoji}) => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    const isSelected = selectedConversation?._id === conversation._id;
     return <>
-    <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+    <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+        ${isSelected ? "bg-sky-500" : ""}
+        `}
+        onClick={() => setSelectedConversation(conversation)}
+        >
         <div className='avatar online'>
             <div className='w-12 rounded-full'>
-                <img src="https://cdn0.iconfinder.com/data/icons/cryptocurrency-137/128/1_profile_user_avatar_account_person-132-512.png" alt='user-avatar'/>
+                <img src={conversation.profilePic} alt='user-avatar'/>
 
             </div>
         </div>
 
     <div className='flex flex-col flex-1'>
         <div className='flex gap-3 justify-between'>
-            <p className='font-bold text-gray-200'>John Doe</p>
-            <span className='text-xl'>&#128512;</span>
+            <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
 
         </div>
 
@@ -24,7 +32,7 @@ const ConvBar = () => {
         
     
     </div>
-    <div className='divider my-0 py-0 h-1' />
+    {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     
     </>;
 };
--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,12 +1,29 @@
 import ConvBar from "./ConvBar.jsx";
+import useGetConversations from "../../hooks/useGetConversatins.js";
+import { getRandomEmoji } from "../../utils/emojis.js";
+
 
 const Conversations = () =>{
+    const { loading, conversations } = useGetConversations();
+    console.log("CONVERSATIONS: ", conversations);
+    //call hook here:
+    useGetConversations();
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <ConvBar/>
-            <ConvBar/>
-            <ConvBar/>
-            <ConvBar/>
+            {conversations.map((conversation,idx) => (
+                <ConvBar 
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.length - 1}
+                
+                
+                />
+
+
+
+            ) )}
+            {loading ? <span className='loading loading-spinner '></span> : null}
         </div>
     );
 

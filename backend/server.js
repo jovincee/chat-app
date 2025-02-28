d320db6c6ac2acd86a504ae17b07dc8a1d68e5d3
John Penaflor <johnvin.penaflor@gmail.com>
2025-02-28 07:16:46 -0700

    Deployment of Chat App

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //npm install express dotenv cookie-parser bcryptjs mongoose socket.io jsonwebtoken
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -13,11 +14,7 @@ dotenv.config();
 
 //environment variable
 const PORT = process.env.PORT || 5000;
-//create a test route, root route which client will be directed to first
-// app.get ("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello World!")
-// });
+const __dirname = path.resolve();
 
 //add another middleware to parse incoming requests, particularly body requests
 app.use(express.json());
@@ -28,11 +25,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+//static middleware; set root directory + frontend + dist (non existent)
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 
-
-
-
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();

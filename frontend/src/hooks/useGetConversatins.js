0b1c932ba7dda3b03a1204c9f0a4cd59f8b1b72c
John Penaflor <johnvin.penaflor@gmail.com>
2025-03-01 20:24:29 -0700

    Updated hooks for getting all chats

--- frontend/src/hooks/useGetConversatins.js
+++ frontend/src/hooks/useGetConversatins.js
@@ -4,6 +4,9 @@ import toast from 'react-hot-toast';
 const useGetConversations = () => {
     const [loading, setLoading] = useState(false);
     const [conversations, setConversations] = useState([]);
+    const [chats, setChats] = useState([]);
+
+    //fetches all users in the system
 
     useEffect(() => {
         const getConversations = async () => {
@@ -26,20 +29,35 @@ const useGetConversations = () => {
 
 
         }
-
         const getChats = async () => {
             setLoading(true);
-            
+            try {
+                let authId = JSON.parse(localStorage.getItem("user-info"))._id;
+                const res = await fetch(`/api/messages/get-msg/${authId}`);
+                const data = await res.json();
+                if (data.error) {
+                    throw new Error(data.error);
+                }
+                setChats(data)
 
 
 
 
-
+            } catch (error) {
+                toast.error(error.message);
+            } finally {
+                setLoading(false);
+            }     
+        
         }
+
+
+
         getConversations();
+        getChats();
 
     },[]);
-    return { loading, conversations }
+    return { loading, conversations, chats }
 };
 
 export default useGetConversations;
